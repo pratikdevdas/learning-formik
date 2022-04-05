0ac0597bdfee1bbc2e1045749367e64678654af4
Revenant <63319865+pratikdevdas@users.noreply.github.com>
2022-04-05 05:45:17 +0530

    visited fields

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const validate = (values) => {
   const errors = {};
   if (!values.firstName) {
     errors.firstName = "Required";
-  } else if (values.firstName.length > 15 || values.firstName.length < 3) {
+  } else if (values.firstName.length > 15 || values.firstName.length < 5) {
     errors.firstName = "Must be more than 5 and less than 15 characters";
   }
 
@@ -43,27 +43,36 @@ const SignupForm = () => {
         name="firstName"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.firstName}
       />
-      {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+      {formik.touched.firstName && formik.errors.firstName ? (
+        <div>{formik.errors.firstName}</div>
+      ) : null}
       <label htmlFor="lastName">Last Name</label>
       <input
         id="lastName"
         name="lastName"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.lastName}
       />
-      {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+      {formik.touched.lastName && formik.errors.lastName ? (
+        <div>{formik.errors.lastName}</div>
+      ) : null}
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
         name="email"
         type="email"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.email}
       />
-      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+      {formik.errors.email && formik.touched.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
 
       <button type="submit">Submit</button>
     </form>
